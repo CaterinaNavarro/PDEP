273179ab501a6f02e939849ce20018463f570e3f
Caterina Elizabeth Navarro <43158195+CaterinaNavarro@users.noreply.github.com>
2019-06-28 22:09:41 -0300

    agregando

--- Prolog/TP 1/democracia.pl
+++ Prolog/TP 1/democracia.pl
@@ -1,9 +1,27 @@
 esCandidatoDelPartido(frank, rojo).
 esCandidatoDelPartido(claire, rojo).
 esCandidatoDelPartido(garrett, azul).
+esCandidatoDelPartido(jackie, amarillo).
+esCandidatoDelPartido(linda, azul).
+esCandidatoDelPartido(catherine, rojo).
+esCandidatoDelPartido(seth, amarillo).
+esCandidatoDelPartido(heather, amarillo).
+
+
+
 
 edadDe(frank, 50).
 edadDe(claire, 52).
 edadDe(garret, 64).
+edadDe(jackie, 38).
+edadDe(peter, 26).
+edadDe(linda, 30).
+edadDe(catherine, 59).
+edadDe(heather, 50).
+
+
+
+
+
 
 
